f5fcd44e79bd6d001363820d16a806aabcd9f821
Ruben Ferreira <rubend192000@gmail.com>
2022-06-22 23:03:10 -0400

    ''Fucked up the models and can't get them back

--- ProyectoSGShoots6/Models/UnidadMedida.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace ProyectoSGShoots6.Models;
-
-public class UnidadMedida
-{
-    [Key] public int Id { get; set; }
-    [Required] public string descripcion { get; set; }
-}
